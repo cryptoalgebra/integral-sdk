f74deb57e52ffe9adb2681173557011d22705bed
lilchizh <xlilchizhx@gmail.com>
2024-10-02 17:34:39 +0300

    v1.2.0

--- src/constants/addresses.ts
+++ src/constants/addresses.ts
@@ -1,9 +1,9 @@
 import { ChainId } from "./chainIds"
 
 export const POOL_DEPLOYER_ADDRESSES = {
-    [ChainId.Holesky]: '0xB9f5Bf5c614AC6D00e973a1828EA63FE4dE50A66',
+    [ChainId.Holesky]: '0x43680a9e859e72158303cEC7F15CE99a04A09674',
 }
 
 export const POOL_INIT_CODE_HASH = {
-    [ChainId.Holesky]: '0x19c31d355e8d7bc982508cd49b44c879eaa0dad74e13aa4f42f1a07accb8cd1c',
+    [ChainId.Holesky]: '0xb3fc09be5eb433d99b1ec89fd8435aaf5ffea75c1879e19028aa2414a14b3c85',
 }
