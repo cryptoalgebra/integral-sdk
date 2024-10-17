2ee292f7e5009965dc1b582da0e4bd251a857d7c
lilchizh <xlilchizhx@gmail.com>
2024-10-17 17:21:35 +0300

    whizzle integral-1.2

--- src/constants/addresses.ts
+++ src/constants/addresses.ts
@@ -1,9 +1,9 @@
 import { ChainId } from "./chainIds"
 
 export const POOL_DEPLOYER_ADDRESSES = {
-    [ChainId.Holesky]: '0x43680a9e859e72158303cEC7F15CE99a04A09674',
+    [ChainId.MorphHolesky]: '0x779166004b1705976b8e31692d9A83D85528AebA',
 }
 
 export const POOL_INIT_CODE_HASH = {
-    [ChainId.Holesky]: '0xb3fc09be5eb433d99b1ec89fd8435aaf5ffea75c1879e19028aa2414a14b3c85',
+    [ChainId.MorphHolesky]: '0xb3fc09be5eb433d99b1ec89fd8435aaf5ffea75c1879e19028aa2414a14b3c85',
 }
--- src/constants/chainIds.ts
+++ src/constants/chainIds.ts
@@ -1,3 +1,3 @@
 export const ChainId = {
-    Holesky: 17000,
+    MorphHolesky: 2810
 }
