b9f5ccd30803895b952f83ca3c567f4833f37351
lilchizh <xlilchizhx@gmail.com>
2025-03-27 14:07:41 +0300

    blackhole deploy

--- src/entities/wnative.ts
+++ src/entities/wnative.ts
@@ -5,9 +5,9 @@ import { Token } from './Token';
  * Known WETH9 implementation addresses, used in our implementation of Ether#wrapped
  */
 export const WNATIVE: { [chainId: number]: Token } = {
-  [ChainId.Holesky]: new Token(
-    ChainId.Holesky,
-    '0x94373a4919b3240d86ea41593d5eba789fef3848',
+  [ChainId.BaseSepolia]: new Token(
+    ChainId.BaseSepolia,
+    '0x4200000000000000000000000000000000000006',
     18,
     'WETH',
     'Wrapped ETH'
