a8947e928bc9150466a744a0468745fbd655c701
lilchizh <xlilchizhx@gmail.com>
2025-07-04 14:10:20 +0700

    babydoge deploy

--- src/constants/addresses.ts
+++ src/constants/addresses.ts
@@ -1,11 +1,9 @@
 import { ChainId } from "./chainIds"
 
 export const POOL_DEPLOYER_ADDRESSES = {
-    [ChainId.Base]: '0x19652b0d7ccddD35E9CC4aCF418556C3Dd0fd31f',
-    [ChainId.BaseSepolia]: '0xFFFE2fCc8e051519bA3B88940D8dB413744d31B5'
+    [ChainId.BscTestnet]: '0x374b74d9E2bb6a1C514e90523208d7b89Fad6a12',
 }
 
 export const POOL_INIT_CODE_HASH = {
-    [ChainId.Base]: '0xb3fc09be5eb433d99b1ec89fd8435aaf5ffea75c1879e19028aa2414a14b3c85',
-    [ChainId.BaseSepolia]: '0xa18736c3ee97fe3c96c9428c0cc2a9116facec18e84f95f9da30543f8238a782'
+    [ChainId.BscTestnet]: '0xa18736c3ee97fe3c96c9428c0cc2a9116facec18e84f95f9da30543f8238a782',
 }
--- src/entities/wnative.ts
+++ src/entities/wnative.ts
@@ -5,16 +5,9 @@ import { Token } from './Token';
  * Known WETH9 implementation addresses, used in our implementation of Ether#wrapped
  */
 export const WNATIVE: { [chainId: number]: Token } = {
-  [ChainId.Base]: new Token(
-    ChainId.Base,
-    '0x4200000000000000000000000000000000000006',
-    18,
-    'WETH',
-    'Wrapped ETH'
-  ),
-  [ChainId.BaseSepolia]: new Token(
-    ChainId.BaseSepolia,
-    '0x4200000000000000000000000000000000000006',
+  [ChainId.BscTestnet]: new Token(
+    ChainId.BscTestnet,
+    '0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd',
     18,
     'WETH',
     'Wrapped ETH'
