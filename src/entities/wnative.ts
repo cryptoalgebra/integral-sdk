51bce268514b0bdeda8136c808cc9b06e87e1b95
lilchizh <xlilchizhx@gmail.com>
2025-07-08 19:53:43 +0700

    change wbtc name

--- src/entities/wnative.ts
+++ src/entities/wnative.ts
@@ -16,14 +16,14 @@ export const WNATIVE: { [chainId: number]: Token } = {
     ChainId.BotanixTestnet,
     '0xcC255E6C96a41152b2E7FB50a7511FBE333c44e9',
     18,
-    'WBTC',
-    'Wrapped BTC'
+    'pBTC',
+    'Pegged Bitcoin'
   ),
   [ChainId.BotanixMainnet]: new Token(
     ChainId.BotanixMainnet,
     '0x0D2437F93Fed6EA64Ef01cCde385FB1263910C56',
     18,
-    'WBTC',
-    'Wrapped BTC'
+    'pBTC',
+    'Pegged Bitcoin'
   )
 };
