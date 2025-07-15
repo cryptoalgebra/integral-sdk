23adff9c49f0d4d922227c1a52fcb05cf55fe424
Kirill S <skdamn@outlook.com>
2025-07-15 21:44:15 +0300

    v1.0.1

--- src/constants/addresses.ts
+++ src/constants/addresses.ts
@@ -1,9 +1,10 @@
-import { ChainId } from "./chainIds"
+import { ChainId } from './chainIds';
 
 export const POOL_DEPLOYER_ADDRESSES = {
-    [ChainId.HyperEvmTestnet]: '0x1f9fc4883492E3d8a4260a8F4Fe45Edb5d4D9040',
-}
+  [ChainId.HyperEvm]: '0x13E7EA93f9b9EA991549334e3f0a1ef46d7C2bFb',
+};
 
 export const POOL_INIT_CODE_HASH = {
-    [ChainId.HyperEvmTestnet]: '0x62441ebe4e4315cf3d49d5957f94d66b253dbabe7006f34ad7f70947e60bf15c',
-}
+  [ChainId.HyperEvm]:
+    '0x62441ebe4e4315cf3d49d5957f94d66b253dbabe7006f34ad7f70947e60bf15c',
+};
--- src/entities/wnative.ts
+++ src/entities/wnative.ts
@@ -1,15 +1,15 @@
-import { ChainId } from "../constants/chainIds";
+import { ChainId } from '../constants/chainIds';
 import { Token } from './Token';
 
 /**
  * Known WETH9 implementation addresses, used in our implementation of Ether#wrapped
  */
 export const WNATIVE: { [chainId: number]: Token } = {
-  [ChainId.HyperEvmTestnet]: new Token(
-    ChainId.HyperEvmTestnet,
+  [ChainId.HyperEvm]: new Token(
+    ChainId.HyperEvm,
     '0x5555555555555555555555555555555555555555',
     18,
     'WHYPE',
     'Wrapped HYPE'
-  )
+  ),
 };
