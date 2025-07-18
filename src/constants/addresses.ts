ce1092b762021f808e3db5e16d3e59babb91ce18
lilchizh <xlilchizhx@gmail.com>
2025-07-18 10:51:33 +0700

    redeploy

--- src/constants/addresses.ts
+++ src/constants/addresses.ts
@@ -1,9 +1,9 @@
 import { ChainId } from "./chainIds"
 
 export const POOL_DEPLOYER_ADDRESSES = {
-    [ChainId.PromMainnet]: '0xd7cB0E0692f2D55A17bA81c1fE5501D66774fC4A',
+    [ChainId.PromMainnet]: '0x16d379f3458bf6DBF6A0dbC5696Be4ab9137cfd4',
 }
 
 export const POOL_INIT_CODE_HASH = {
-    [ChainId.PromMainnet]: '0xa18736c3ee97fe3c96c9428c0cc2a9116facec18e84f95f9da30543f8238a782',
+    [ChainId.PromMainnet]: '0x62441ebe4e4315cf3d49d5957f94d66b253dbabe7006f34ad7f70947e60bf15c',
 }
