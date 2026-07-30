1a6bc9dea56dd94ac98f7bdead459905d09bfa82
damnnou <skdamn@outlook.com>
2026-07-30 11:33:34 +0300

    v1.0.0 (deploy on alpen testnet)

--- src/constants/wnative.ts
+++ src/constants/wnative.ts
@@ -5,11 +5,11 @@ import { Token } from '../entities/Token';
  * Known WETH9 implementation addresses, used in our implementation of Ether#wrapped
  */
 export const WNATIVE: { [chainId: number]: Token } = {
-  [ChainId.BaseSepolia]: new Token(
-    ChainId.BaseSepolia,
-    '0x4200000000000000000000000000000000000006',
+  [ChainId.AlpenTestnet]: new Token(
+    ChainId.AlpenTestnet,
+    '0x10253594A832f967994b44f33411940533302ACb',
     18,
-    'WETH',
-    'Wrapped ETH'
+    'WsBTC',
+    'Wrapped Signet BTC'
   ),
 };
